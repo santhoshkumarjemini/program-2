63b5773050f2ca27c0600520b02b206432710304
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-05-14 11:48:41 +0530

    Adddot.java

---
+++ Adddot.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Adddot
+{
+ public static void main(String args[])
+ {
+  String n;
+  System.out.println("Input");
+  Scanner in=new Scanner(System.in);
+  n=in.nextLine();
+  System.out.println(n+".");
+ }
+}
